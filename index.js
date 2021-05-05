a6e5395372ad96b55421e6278bbac45a45016cab
Alexey Mazheykin <mazheykin@icloud.com>
2021-05-05 21:22:19 +0300

    first

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+<div className="modal-overlay" data-close="true">
+    <div className="modal-window" style="width: ${option.width || DEFAULT_WIDTH}">
+        <div className="modal-header">
+                <span className="modal-title">
+                    ${option.title || 'Default title'}
+                </span>
+            ${option.closable ? '<span class="modal-close" data-close="true">&times;</span>' : ''}
+        </div>
+        <div className="modal-body" data-content>
+            ${option.content || 'no content'}
+        </div>
+    </div>
+</div>
+
 const myOptionObj = {
     title: 'Dunhills Title',
     content: `
